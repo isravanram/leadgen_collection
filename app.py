d0f7d58df178858790052b58b645300b062410bb
isravanram <isravanram@gmail.com>
2024-12-10 11:43:10 +0400

    Exported secrets using env variables

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from airtable import Airtable
 
 app = Flask(__name__)
 
-#keys
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+APOLLO_API_KEY = os.getenv("APOLLO_API_KEY")
 
 AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 AIRTABLE_TABLE_NAME = os.getenv("AIRTABLE_TABLE_NAME")
@@ -27,17 +27,18 @@ def export_to_airtable(table_object,data):
 
 def people_enrichment(apollo_id):
     print(f"\n------------Started People Enrichment API------------")
-    url = f"https://api.apollo.io/api/v1/people/match?id={apollo_id}&reveal_personal_emails=false&reveal_phone_number=false"
+    url = f"https://api.apollo.io/api/v1/mixed_people/search?person_titles[]=marketing%20manager&person_titles[]=marketing%20director&person_locations[]=Dubai%2C%20United%20Arab%20Emirates&person_seniorities[]=ceo&person_seniorities[]=cmo&person_seniorities[]=director&organization_locations[]=Dubai%2C%20United%20Arab%20Emirates&contact_email_status[]=verified&contact_email_status[]=likely%20to%20engage&organization_num_employees_ranges[]=1%2C10&organization_num_employees_ranges[]=11%2C20&organization_num_employees_ranges[]=21%2C50&page={page_number}&per_page={results_per_page}"
 
     headers = {
         "accept": "application/json",
         "Cache-Control": "no-cache",
         "Content-Type": "application/json",
-        "x-api-key": "ti7hoaAD9sOc5DGzqZUk-Q"
+        "x-api-key": APOLLO_API_KEY
     }
+
     response = requests.post(url, headers=headers)
     print(response.json())
-    print(f"------------Completed People Enrichment API------------\n")
+    print(f"------------Completed People Enrichment API------------")
     return response
 
 
@@ -62,24 +63,20 @@ def people_search(page_number,results_per_page):
           column_name = 'id'  # Column name to search in
           unique_value = apollo_id  # The value you want to omit
     
-          table = table_object
-
           # Retrieve all records from the table
-          records = table.all()
-          print(f'records : {records}')
-          # Filter out the records where the value in the column matches the value to omit
+          records = airtable_obj.all()
+        #   print(f'records : {records}')
 
+          # Filter out the records where the value in the column matches the value to omit
           record_exists = any(record['fields'].get(column_name) == unique_value for record in records)    
           if record_exists:
-            print(f'Record {apollo_id} already exists...skipping')
+            print(f'Record {apollo_id} already exists. Skipping the entry...')
             continue   
 
           enrichment_api_response = people_enrichment(apollo_id)
           if enrichment_api_response.status_code == 200:
               data = enrichment_api_response.json()
-              # print(data)
               data=data['person']
-              print(data['employment_history'])
               response = openai.ChatCompletion.create(
               model="gpt-3.5-turbo",  # or "gpt-4" for more advanced results
               messages=[
@@ -130,21 +127,24 @@ def people_search(page_number,results_per_page):
               print('~~~~~~~~~People enrichment successful~~~~~~~~~~~~')
           else:
               print(f"Error: {enrichment_api_response.status_code}, People Enrichment API failed")
+              return False
+      return True  
   else:
       print("People Search API failed.")
+      return False
 
 
 @app.route("/enrich_people")
 def execute_collection():
-  page_number = 43
-  results_per_page = 1
-  job_titles = []
-  locations = []
-  organization_num_employees_ranges = [1,10]
-  seniority = []
-  email_type = []
-  people_search(page_number,results_per_page)
-  return 'Successful'
+  page_number = 2
+  results_per_page = 2
+#   job_titles = []
+#   locations = []
+#   organization_num_employees_ranges = [1,10]
+#   seniority = []
+#   email_type = []
+  success_status = people_search(page_number,results_per_page)
+  return 'Successfully collected the data' if success_status else 'Failed retrieving information from Apollo.'
 
 if __name__ == '__main__':
   app.run(debug=True)
